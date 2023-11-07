dfc37205125539b87375101e3e7da080b6f6ef23
timirichy <tianchris198@gmail.com>
2023-11-07 20:03:30 +0100

    js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,12 +1,16 @@
-#!/usr/bin/node
+#!/usr/bin/node | chmod a+x 103-object_fct.js
+
 const myObject = {
-    type: 'object',
-    value: 12
+  type: "object",
+  value: 12,
 };
 console.log(myObject);
 
+// Add the "incr" method to the "myObject" object.
 myObject.incr = function () {
+  if (typeof this.value === "number") {
     this.value++;
+  }
 };
 
 myObject.incr();
